7a48bfd33cc06199bfec6e726e55548b19e5fc12
Sarah Rabah <126303696+srab017@users.noreply.github.com>
2024-07-07 18:31:55 +1200

    Syntax error fixed  index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// Select the correct elements
 const cells = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#statusText");
 const restartbtn = document.querySelector("#restartbtn");
-//winning conditions
+
+// Define winning conditions
 const winConditions = [
-    [0, 1, 2]
+    [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
@@ -11,76 +13,77 @@ const winConditions = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-    
 ];
 
-let options = ["", "", "", "", "", "", "", "", ""]; 
-let currentPlayer = "X"; 
-let running =  flase; 
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
 
-initilizeGame(); 
+initializeGame();
 
-function initilizeGame(){
+function initializeGame() {
     cells.forEach(cell => cell.addEventListener("click", cellClicked));
     restartbtn.addEventListener("click", restartGame);
     statusText.textContent = `${currentPlayer}'s turn`;
     running = true;
 }
-function cellClicked(){
-    const cellIndex = this.getAttribute("cellIndex"); 
 
-    if(options[cellIndex] != "" || !running){
+function cellClicked() {
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if (options[cellIndex] != "" || !running) {
         return;
-        
     }
 
     updateCell(this, cellIndex);
     checkWinner();
 }
-function updateCell(cell, index){
+
+function updateCell(cell, index) {
     options[index] = currentPlayer;
     cell.textContent = currentPlayer;
 }
-function changePlayer (){
-    currentPlayer = (currentPlayer == "X") ? "O" : "X"; 
+
+function changePlayer() {
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
     statusText.textContent = `${currentPlayer}'s turn`;
-    
 }
-function checkWinner(){
+
+function checkWinner() {
     let roundWon = false;
 
-    for(let i = 0; i < winConditions.length; i++){
-        const conditions = winConditions[i];
+    for (let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
         const cellA = options[condition[0]];
         const cellB = options[condition[1]];
-        const cellC = options[condittion[2]];
+        const cellC = options[condition[2]];
 
-        // check if there are empty spaces 
-        if(cellA == "" || cellB == "" || cellC == ""){
+        // Check if there are empty spaces
+        if (cellA == "" || cellB == "" || cellC == "") {
             continue;
         }
 
-        // make sure they all the same characters
-        if(cellA == cellB && cellB == cellC){
-            roundWon = ture;
+        // Make sure they all have the same character
+        if (cellA == cellB && cellB == cellC) {
+            roundWon = true;
             break;
         }
-
     }
 
-    if(roundWon){
-        statusText.textContent = `${currentPlayer} WINS`;
+    if (roundWon) {
+        statusText.textContent = `${currentPlayer} WINS!`;
         running = false;
-    }else if(!options.includes("")){
+    } else if (!options.includes("")) {
         statusText.textContent = `DRAW!`;
         running = false;
-    }else{
+    } else {
         changePlayer();
     }
 }
-function restartGame(){
-    currentPlayer = "X"; 
-    options = ["", "", "", "", "", "", "", "", ""]; 
+
+function restartGame() {
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
     statusText.textContent = `${currentPlayer}'s turn`;
     cells.forEach(cell => cell.textContent = "");
     running = true;
